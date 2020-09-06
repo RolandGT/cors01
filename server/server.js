47284defb37b1b762cf05cebedb4f9e4ffb6a655
RolandGT <rolandas.get@gmail.com>
2020-09-07 00:25:43 +0200

    first commit

--- server/server.js
+++ server/server.js
@@ -5,9 +5,9 @@ const publicPath = path.join(__dirname, '..', 'public');
 const port = process.env.PORT || 3000;
 
 app.use(express.static(publicPath));
-app.get('*', (req, res) => {
-   res.sendFile(path.join(publicPath, 'index.html'));
-});
+// app.get('*', (req, res) => {
+//    res.sendFile(path.join(publicPath, 'index.html'));
+// });
 app.listen(port, () => {
    console.log('Server is up!');
 });
